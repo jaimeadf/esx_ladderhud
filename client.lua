6217f7d12f671fdd130f59658483537916b94121
MarmotaGit <jaimeadanielfilho@hotmail.com>
2018-12-05 21:02:10 -0200

    Solved another little bug!

--- client.lua
+++ client.lua
@@ -14,7 +14,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ]]
 
-local ESX            = nil
+ESX            = nil
 local thirst, hunger = 0, 0
 
 Citizen.CreateThread(function()
